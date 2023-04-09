48e7677abccdb206aacc2758de5345ccff29d9fe
Leo Spratt <enchant97@users.noreply.github.com>
2023-04-09 17:50:49 +0100

    implement consumer+producer

--- config/config.go
+++ config/config.go
@@ -1,5 +1,10 @@
 package config
 
+type AMPQConfig struct {
+	URI       string `env:"URI,notEmpty"`
+	QueueName string `env:"QUEUE_NAME,notEmpty"`
+}
+
 type ImageSizes struct {
 	Large     uint `env:"LARGE" envDefault:"2500"`
 	Medium    uint `env:"MED" envDefault:"1000"`
@@ -8,7 +13,11 @@ type ImageSizes struct {
 }
 
 type AppConfig struct {
+	AMPQConfig    AMPQConfig `envPrefix:"AMPQ__"`
 	ImageSizes    ImageSizes `envPrefix:"IMAGE_SIZES__"`
 	OriginalsPath string     `env:"ORIGINALS_PATH,notEmpty"`
 	OptimizedPath string     `env:"OPTIMIZED_PATH,notEmpty"`
+	Publisher     bool       `env:"PUBLISHER" envDefault:"false"`
+	Consumer      bool       `env:"CONSUMER" envDefault:"false"`
+	PublisherScan bool       `env:"PUBLISHER_SCAN" envDefault:"false"`
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.20
 require (
 	github.com/caarlos0/env/v6 v6.10.1
 	github.com/h2non/bimg v1.1.9
+	github.com/rabbitmq/amqp091-go v1.8.0
 )
